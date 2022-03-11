cb543b753364b4e41c827c076c696e21883ca864
mynthan18 <18mynthan@gmail.com>
2022-03-10 19:04:48 -0800

    Shalinigopal

---
+++ for.java
@@ -0,0 +1,15 @@
+class exam
+{
+public static void main(String argv[])
+{
+for(int i=1;i<=4;i++)
+{
+for(int j=1;j<=4;j++)
+{
+System.out.print("*");
+if(i==j)break;
+}
+System.out.println();
+}
+}
+}
